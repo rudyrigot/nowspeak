a5d8e07f69c51c01133e39557fbc49115b67b3ad
Rudy Rigot <rudy.rigot@gmail.com>
2014-01-30 18:06:26 -0800

    Cleaning things up, ready to start

---
+++ js/nowspeak-configuration.js
@@ -0,0 +1,3 @@
+/* Parse configuration */
+var parseClientID = "XwZsy00ka18Z2Gg2DIdPOf411oYwBu2yn0U5NJJO";
+var parseJavascriptKey = "J4KByQqc2rWicfbM1qYrtFPCU6iWZadPf0OO98IO";
---
+++ js/nowspeak.js
@@ -0,0 +1,13 @@
+/* Initializing Parse */
+Parse.initialize(parseClientID, parseJavascriptKey);
+
+// var TestObject = Parse.Object.extend("TestObject");
+// var testObject = new TestObject();
+// testObject.save({foo: "bar"}, {
+//   success: function(object) {
+//     $(".success").show();
+//   },
+//   error: function(model, error) {
+//     $(".error").show();
+//   }
+// });
