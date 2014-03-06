d3c0dbc4e9b2583e734bd708b69aa809eda1699a
Rudy Rigot <rudy.rigot@gmail.com>
2014-03-05 23:49:58 -0800

    Fixing the error message if browser doesn't support Speech API

--- js/nowspeak-i18n.en.js
+++ js/nowspeak-i18n.en.js
@@ -5,7 +5,7 @@ var i18n_en = {
 	bottombar_hit_space : 'Hit the spacebar, and speak',
 	bottombar_recording : 'Recording',
 
-	error_requirements_speech : 'Oops, it seems like your browser doesn\'t handle speech recognition. It\'s not that surprising, it\'s a pretty recent feature. Upgrade your browser, or switch to another one!',
+	error_requirements_speech : 'Oops, it seems like your browser doesn\'t handle speech recognition. It\'s not that surprising, it\'s a pretty recent feature, and only Google Chrome supports it for now. Try this on Google Chrome!',
 	error_recording : 'Error while recording',
 	error_recording_by_code : {
 		'no-speech' : 'It sounds like you said nothing; can you get closer to the mic?',
